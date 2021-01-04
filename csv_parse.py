1cc5d2a6ef95c21c43d4bd3615d6bb3e6ff6121f
ramesh6666 <ramesh6@yahoo.com>
2021-01-03 16:19:54 -0800

    Add files via upload

---
+++ csv_parse.py
@@ -0,0 +1,43 @@
+import os
+import re
+import sys
+import urllib.request
+import shutil
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+  f = open("/Users/rameshiyerbalan/Documents/electronic-card-transactions-november-2020-csv-tables.csv", "r")
+  csv_reader = csv.reader(f, delimiter=',')
+  for row in csv_reader:
+      print(len(row))
+      break
+  df = pd.read_csv("/Users/rameshiyerbalan/Documents/electronic-card-transactions-november-2020-csv-tables.csv", nrows = 2)
+  for dtype in df.dtypes.iteritems():
+    print(dtype)
+
+  print(df)
+  df = pd.read_csv("/Users/rameshiyerbalan/Documents/electronic-card-transactions-november-2020-csv-tables.csv", parse_dates=['Period'])
+  print(df[0:5])
+  total_rows = df.count
+  print(total_rows)
+  print('HELLO \n')
+  print('Mean = ', df["Data_value"].mean())
+  print(df['Period'])
+  print(df['Data_value'])
+  df.plot(x="Period", y="Data_value")
+  plt.show()
+
+#  dict = {}
+#  for row in csv_reader:
+#    year = row[1].split('.')
+#    y = year[0]
+#    val = row[2]
+#    dict[y] = val
+
+#  print(len(dict))
+#  print(dict)
+
+if __name__ == '__main__':
+  main()
